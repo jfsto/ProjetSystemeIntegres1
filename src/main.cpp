59aac1d0a7b11b7c9027c98310312d4c0cd5af4a
jfsto <jeanfrancois.santoro@hotmail.com>
2022-10-11 21:26:12 +0200

    test git

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 4; i++)
+  {
+    Serial.println("Hello World");
+    delay(1000);
+  }
+  
+  Serial.end();
+  
+  vvvvvv
+  gfgfg
+  
+}
+
