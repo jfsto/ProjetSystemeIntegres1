e38b12865696518d75e3aa768ac879e696dbd7f2
jfsto <jeanfrancois.santoro@hotmail.com>
2022-10-21 16:10:26 +0200

    V1 web

---
+++ data/script.js
@@ -0,0 +1,8 @@
+function autobtn(){
+    document.getElementById("valeurmode").innerHTML="Automatique"
+   
+}
+
+function manubtn(){
+    document.getElementById("valeurmode").innerHTML="Manuel"
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
 
+
+const char* ssid     = "RobotGouttières";
+const char* password = "robotgouttieres";
+
+AsyncWebServer server(80);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+   Serial.begin(9600);
+
+  //....Setup Access Point Wifi
+  Serial.println("\n[*] Creating AP");
+  // WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+   // Serial.print("[+] AP Created with IP Gateway ");
+   // Serial.println(WiFi.softAPIP());
+
+  //.....Setup SPIFFS
+    if(!SPIFFS.begin())
+  {
+    Serial.println("Erreur SPIFFS...");
+    return;
+  }
+
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+
+  while(file)
+  {
+    Serial.print("File: ");
+    Serial.println(file.name());
+    file.close();
+    file = root.openNextFile();
+  }
+
+    //----------------------------------------------------SERVER
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+   server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/script.js", "text/javascript");
+  });
+
+server.begin();
 
 }
 
@@ -17,7 +68,7 @@ void loop() {
   
   Serial.end();
   
-  
+
   
 }
 
